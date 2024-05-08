4150e18f3a2e6c111bd53619c3f2bbe3d4884f29
CaesarMundi <126364879+CaesarMundi@users.noreply.github.com>
2024-05-07 23:54:36 -0400

    Added spacing

--- air_quality.py
+++ air_quality.py
@@ -36,5 +36,5 @@ x = distToSens(1,1)
 print(x)
 
 # aqi_dist = (dist2 + dist3) / 2 / (dist1 + dist2 + dist3) * AQI1 + (dist1 + dist3) / 2 / (dist1 + dist2 + dist3) * AQI2 + (dist1 + dist2) / 2 / (dist1 +dist2 + dist3) * AQI3
-# aqi2 = 0.2*aqi_temp+0.8*aqi_dist
+# aqi2 = 0.2 * aqi_temp + 0.8 * aqi_dist
 # print(aqi2)
